783de6a0b9b01a11ba5f30fb5b47f05f4b7d6166
Prince Appiah <pappiah00@gmail.com>
2021-09-20 23:05:52 +0000

    Update .gitignore

---
+++ src/config/db.config.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
