f4359288d3a0364f4183343c04a921cc901fbe53
Prince Appiah <pappiah00@gmail.com>
2021-09-21 14:42:30 +0000

    Testing deployment

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(
   })
 );
 
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, HOST, async () => {
