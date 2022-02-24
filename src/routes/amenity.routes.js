75dda4e2da8746871166de432e6a69fd6b0a83ff
Prince Appiah <pappiah00@gmail.com>
2022-02-24 12:58:29 +0000

    feat: :sparkles: Added jwt configuration and middleware to allow roles

--- src/routes/amenity.routes.js
+++ src/routes/amenity.routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { roles } = require("../config/constants");
 const AmenityController = require("../controllers/amenity.controller");
+const allowRoles = require("../middlewares/allowRoles");
+const requireToken = require("../middlewares/requireToken");
 
 module.exports = (app) => {
   let router = express.Router();
 
-  router.get("/amenities", AmenityController.listAll);
+  router.get(
+    "/amenities",
+    requireToken,
+    allowRoles([roles.USER]),
+    AmenityController.listAll
+  );
+
   router.post("/amenities", AmenityController.create);
   router.patch("/amenities/:id", AmenityController.update);
   router.delete("/amenities/:id", AmenityController.remove);
