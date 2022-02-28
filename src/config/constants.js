0a48202b8841a82a4388a9b081d4bc8a88c18e6f
Prince <pappiah00@gmail.com>
2022-02-28 16:39:14 +0000

    refactor: :recycle: Update database connection

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,8 +4,14 @@ module.exports = {
     ADMIN: "admin",
     USER: "user",
   },
-    mailGun: {
+  mailGun: {
     API_KEY: process.env.MAILGUN_API_KEY,
     DOMAIN: process.env.MAILGUN_DOMAIN,
   },
- }; 
+  db: {
+    USER: process.env.DB_USER,
+    PASS: process.env.DB_PASSWORD,
+    NAME: process.env.DB_NAME,
+    URL: `mongodb+srv://${USER}:${PASS}@sheltercluser.khpt7.mongodb.net/${NAME}?retryWrites=true&w=majority`,
+  },
+};
