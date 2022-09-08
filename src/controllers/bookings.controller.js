aa2194915124697df26b69355dcda082336e2967
Prince Appiah <pappiah00@gmail.com>
2022-09-08 15:15:50 +0000

    feat: :sparkles: Added endpoints to cancel and check booked property

--- src/controllers/bookings.controller.js
+++ src/controllers/bookings.controller.js
@@ -11,12 +11,55 @@ class BookingController {
       return res.status(500).json(error);
     }
   }
+
   static async getCustomerBookings(req, res) {
     try {
       const user_id = req.user._id;
       console.log("🚀 ~ user_id", user_id);
 
       const response = await BookingRepo.getCustomerBookings({ user_id });
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log("🚀 ~ error", error);
+      Sentry.captureException(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  static async checkBookedProperty(req, res) {
+    try {
+      const user_id = req.user._id;
+      const { property_id } = req.params;
+
+      // search through the booking model(match customer and property ids) for the property id passed in the request
+      const response = await BookingRepo.checkBookedProperty({
+        user_id,
+        property_id,
+      });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log("🚀 ~ error", error);
+      Sentry.captureException(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  static async cancelBooking(req, res) {
+    try {
+      const user_id = req.user._id;
+      const { property_id } = req.params;
+
+      if (!property_id) {
+        return res.status(400).json({ msg: "Property ID is required" });
+      }
+
+      // search through the booking model(match customer and property ids) for the property id passed in the request
+      const response = await BookingRepo.cancelBooking({
+        user_id,
+        property_id,
+      });
+
       return res.status(200).json(response);
     } catch (error) {
       console.log("🚀 ~ error", error);
